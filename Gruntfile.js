34e017ae9d14faffd8445500b61889b769a054f5
sam <sam@192-168-1-5.tpgi.com.au>
2020-02-27 17:30:46 +1000

    update

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+    		connect: {
+    			sever: {
+    				options: {
+    					hostname: '192.168.1.5',
+    					port: 3000,
+    					base: '',
+    					livereload: true
+    				}
+    			}
+    			},
+
+    			watch: {
+    				options: {
+    					livereload: true,
+    					dateFormat: function(time) {
+    						grunt.log.writeln('The watch finished in ' + time + 'ms at' + (new Date()).toString());
+    						grunt.log.writeln('Waiting for more changes...');
+    					}
+    					},
+    					scripts: {
+    						files: '**/**.js',
+    						},
+              html: {
+    						files: '**/**.html',
+    						},
+    					css: {
+    						files: ['**/**.css'],
+    						}
+    					}
+
+
+  }); //initConfig
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.registerTask('default', ['connect', 'watch']);
+
+}; //wrapper function
